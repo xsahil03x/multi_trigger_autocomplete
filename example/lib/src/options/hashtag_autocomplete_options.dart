0377e910f9e0fd066bbee808626bcf3de17eb9c5
Bruno Stone <brnostone@gmail.com>
2023-05-25 22:01:23 -0300

    add regex pattern in trigger and an example of async data search

--- example/lib/src/options/hashtag_autocomplete_options.dart
+++ example/lib/src/options/hashtag_autocomplete_options.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:example/src/models.dart';
 
-class HashtagAutocompleteOptions extends StatelessWidget {
+class HashtagAutocompleteOptions extends StatefulWidget {
   const HashtagAutocompleteOptions({
     Key? key,
     required this.query,
@@ -14,15 +14,49 @@ class HashtagAutocompleteOptions extends StatelessWidget {
   final ValueSetter<Hashtag> onHashtagTap;
 
   @override
-  Widget build(BuildContext context) {
-    final hashtags = kHashtags.where((it) {
+  State<HashtagAutocompleteOptions> createState() =>
+      _HashtagAutocompleteOptionsState();
+}
+
+class _HashtagAutocompleteOptionsState
+    extends State<HashtagAutocompleteOptions> {
+  bool _isLoading = false;
+  Iterable<Hashtag> _items = List.empty();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _search();
+  }
+
+  @override
+  void didUpdateWidget(HashtagAutocompleteOptions oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    _search();
+  }
+
+  Future<void> _search() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    _items = kHashtags.where((it) {
       final normalizedOption = it.name.toLowerCase();
-      final normalizedQuery = query.toLowerCase();
+      final normalizedQuery = widget.query.toLowerCase();
       return normalizedOption.contains(normalizedQuery);
     });
 
-    if (hashtags.isEmpty) return const SizedBox.shrink();
+    _isLoading = false;
 
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(8),
       elevation: 2,
@@ -38,41 +72,58 @@ class HashtagAutocompleteOptions extends StatelessWidget {
             child: ListTile(
               dense: true,
               horizontalTitleGap: 0,
-              title: Text("Hashtags matching '$query'"),
+              title: Text("Hashtags matching '${widget.query}'"),
             ),
           ),
           const Divider(height: 0),
           LimitedBox(
             maxHeight: MediaQuery.of(context).size.height * 0.3,
-            child: ListView.separated(
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
-              itemCount: hashtags.length,
-              separatorBuilder: (_, __) => const Divider(height: 0),
-              itemBuilder: (context, i) {
-                final hashtag = hashtags.elementAt(i);
-                return ListTile(
-                  dense: true,
-                  leading: CircleAvatar(
-                    backgroundColor: const Color(0xFFF7F7F8),
-                    backgroundImage: NetworkImage(
-                      hashtag.image,
-                      scale: 0.5,
-                    ),
-                  ),
-                  title: Text('#${hashtag.name}'),
-                  subtitle: Text(
-                    hashtag.description,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () => onHashtagTap(hashtag),
-                );
-              },
-            ),
+            child: (_isLoading) ? _buildLoader() : _buildList(),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildLoader() {
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: SizedBox(
+        width: 24,
+        height: 24,
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.separated(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: _items.length,
+      separatorBuilder: (_, __) => const Divider(height: 0),
+      itemBuilder: (context, i) {
+        final hashtag = _items.elementAt(i);
+        return ListTile(
+          dense: true,
+          leading: CircleAvatar(
+            backgroundColor: const Color(0xFFF7F7F8),
+            backgroundImage: NetworkImage(
+              hashtag.image,
+              scale: 0.5,
+            ),
+          ),
+          title: Text('#${hashtag.name}'),
+          subtitle: Text(
+            hashtag.description,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          onTap: () => widget.onHashtagTap(hashtag),
+        );
+      },
     );
   }
 }
